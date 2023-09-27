115fb94c0e96436482582dfb34699ca77d9f9d59
Allan LeSage <alesage@fsi.io>
2023-09-27 19:58:31 +0000

    Changelog entry and version bump for 0.3.0.

--- version.go
+++ version.go
@@ -1,4 +1,5 @@
 /*
+ * Copyright (c) 2023 DomainTools LLC
  * Copyright (c) 2018 Farsight Security, Inc.
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
@@ -8,4 +9,4 @@
 
 package main
 
-var Version = "0.2.0"
+var Version = "0.3.0"
